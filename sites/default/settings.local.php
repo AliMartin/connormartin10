506234f59cb5b41e6b9936e59fd31fb086557c63
Alistair Martin <2270211+AliMartin@users.noreply.github.com>
2024-10-21 09:55:15 +0100

    Beta launch

---
+++ sites/default/settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+$settings['update_free_access'] = FALSE;
+$settings['file_private_path'] = DRUPAL_ROOT . '/private-files';
+$settings['file_temp_path'] = DRUPAL_ROOT . '/tmp';
+$settings['config_sync_directory'] = DRUPAL_ROOT . '/sync';
+
+$settings['trusted_host_patterns'] = [
+  '^connormartin\.co\.uk$',
+  '^.+\.connormartin\.co\.uk$',
+  '^connormartin10\.ddev\.site$',
+  '^.+\.connormartin10\.ddev\.site$',
+];
+
+
